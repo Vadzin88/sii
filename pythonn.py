2c50a4ea464ab57de7f83fb18beb8a2dedfabf4c
Vadzin88 <davi20747@gmail.com>
2022-10-15 16:50:53 +0000

    Mudanças

---
+++ pythonn.py
@@ -0,0 +1,1 @@
+print("Eu sou o milhor, posso não ser, mais na minha cabeça eu sou o melhor!!!!")
---
+++ sla.java
@@ -0,0 +1,20 @@
+import javax.swing.*;
+import java.awt.*;
+
+class Main extends JFrame{
+  public static void main(String[] args) {
+    Main janela = new Main();
+    janela.setSize(495, 245);
+    janela.setResizable(false);
+    janela.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    janela.setVisible(true);
+
+  }
+
+  public void paint(Graphics g) {
+    g.setColor(Color.RED);
+    g.fillRect(0,0,495,245);
+    g.setColor(Color.BLUE);
+    g.drawLine(0,0,495,245);
+  }
+}
